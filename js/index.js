391c77d2638962538c3e6ace2461f514d882ccb7
Shyam Prasad M <106013711+PegaShyam@users.noreply.github.com>
2023-10-04 21:29:17 +0530

    Added Files for Hosting

---
+++ js/index.js
@@ -0,0 +1,53 @@
+// DYNAMIC TEXT
+
+const dynamicText = document.querySelector("h1 span");
+const words = ["Front-End Developer."];
+// Variables to track the position and deletion status of the word
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+    dynamicText.classList.add("stop-blinking");
+    if (!isDeleting && charIndex < currentWord.length) {
+        // If condition is true, type the next character
+        charIndex++;
+        setTimeout(typeEffect, 200);
+    } else if (isDeleting && charIndex > 0) {
+        // If condition is true, remove the previous character
+        charIndex--;
+        setTimeout(typeEffect, 100);
+    } else {
+        // If word is deleted then switch to the next word
+        isDeleting = !isDeleting;
+        dynamicText.classList.remove("stop-blinking");
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
+    }
+}
+typeEffect();
+
+// NAVBAR
+
+const navigation = document.getElementById("nav");
+const menu = document.getElementById("menu");
+
+menu.addEventListener("click", () => {
+  // The navigation.children.length means the following :-
+  // The children inside a parent are basically an array of elements; So, here I'm finding the length of the array aka how many children are inside the nav bar.
+  //   Yup That's all.
+  navigation.style.setProperty("--childenNumber", navigation.children.length);
+
+  //    Casually Toggling Classes to make them animate on click
+  
+  navigation.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+// CUBE ROTATE ON SCROLL
+
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  }, false);
